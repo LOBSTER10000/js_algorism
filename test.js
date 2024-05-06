c2a0063a5f7121b1653122fd11d1e9ba7ca0fb99
LOBSTER10000 <111476138+LOBSTER10000@users.noreply.github.com>
2024-05-06 18:38:56 +0900

    12917

--- test.js
+++ test.js
@@ -1,15 +1,6 @@
 let array = [7,4,6,7,1,2,335,6,7,8];
 
-
-
-
-
-let min = Math.min(...array);
-console.log(min);
-
-let arr1 = array.slice(0, array.indexOf(min));
-let arr2 = array.slice(array.indexOf(min)+1, array.length);
-let arr3 = arr1.concat(arr2);
+array.splice(array.indexOf(Math.min(...array)), 1);
 // console.log(arr1)
 // console.log(arr2);
-console.log(arr3);
+console.log(array);
