3350f5c0fae233beaa2071c05bc0fb75efea1a13
LOBSTER10000 <111476138+LOBSTER10000@users.noreply.github.com>
2024-05-17 23:04:50 +0900

    147355

--- programmers147355.js
+++ programmers147355.js
@@ -1,14 +1,15 @@
+
 function solution(t, p) {
     var array = [];
     var answer = 0;
     var lengths = p.split('').length;
     var topos = t.split('');
-    
+
     for(let i= 0; i < t.length; i++){
         array = [];
         array = topos.slice(i, i+lengths);
         array = array.join('')
-        
+
         if(parseInt(array) <= parseInt(p)){
             if(array.length == lengths){
             answer++;
